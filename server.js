ce5d0cecd2295d123c5bfa312ce97c262480cf1e
cdgrody <cdgrody@gmail.com>
2023-01-21 14:15:03 -0800

    more google auth complete

--- server.js
+++ server.js
@@ -22,6 +22,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true
+}))
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
