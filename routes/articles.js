cfabe1be96fc6ab7c5baa70a6367536825fc120f
cdgrody <cdgrody@gmail.com>
2023-01-23 16:20:32 -0800

    Authorization is now required for adding, editing, and deleting all content from the website

--- routes/articles.js
+++ routes/articles.js
@@ -1,14 +1,15 @@
 var express = require('express');
 var router = express.Router();
 const articlesCtrl = require('../controllers/articles')
+const ensureLoggedIn = require('../config/ensureLoggedIn')
 
 /* GET users listing. */
-router.get('/new', articlesCtrl.new);
-router.post('/', articlesCtrl.create);
+router.get('/new', ensureLoggedIn, articlesCtrl.new);
+router.post('/', ensureLoggedIn, articlesCtrl.create);
 router.get('/', articlesCtrl.index);
-router.delete('/:id', articlesCtrl.delete);
-router.get('/:id/edit', articlesCtrl.edit);
-router.put("/:id", articlesCtrl.update)
+router.delete('/:id', ensureLoggedIn, articlesCtrl.delete);
+router.get('/:id/edit', ensureLoggedIn, articlesCtrl.edit);
+router.put("/:id", ensureLoggedIn, articlesCtrl.update)
 router.get('/:id', articlesCtrl.show);
 //get /articles/:id/edit
 
--- routes/comments.js
+++ routes/comments.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 const commentsCtrl = require('../controllers/comments')
+const ensureLoggedIn = require('../config/ensureLoggedIn')
 
 /* GET users listing. */
-router.post('/articles/:id/comments', commentsCtrl.create);
+router.post('/articles/:id/comments', ensureLoggedIn, commentsCtrl.create);
 
 //POST /articles/:id/comments
 
