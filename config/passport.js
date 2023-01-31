4f3e205b7b7d6932f6ba991610c931970d50cab7
cdgrody <cdgrody@gmail.com>
2023-01-31 13:09:41 -0800

    Error of new accounts not being able pass authentication has now been resolved

--- config/passport.js
+++ config/passport.js
@@ -10,19 +10,19 @@ passport.use(new GoogleStrategy(
     },
     async function(accessToken, refreshToken, profile, cb) {
         try {
-            let user = await User.findOne({googleId: profile.id})
-            if (user) return cb(null, user)
-            user = await User.create({
-                name: profile.displayName,
-                googleId: profile.id,
-                email: profile.emails[0].value,
-                avatar: profile.photo[0].value
-            });
-            return cb(null, user);
-        } catch(err) {
-            return cb(err);
+          let user = await User.findOne({ googleId: profile.id });
+          if (user) return cb(null, user);
+          user = await User.create({
+            name: profile.displayName,
+            googleId: profile.id,
+            email: profile.emails[0].value,
+            avatar: profile.photos[0].value
+          });
+          return cb(null, user);
+        } catch (err) {
+          return cb(err);
         }
-    }
+      }
 ));
 
 passport.serializeUser(function(user, cb) {
